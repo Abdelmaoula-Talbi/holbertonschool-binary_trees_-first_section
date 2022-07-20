64733aabfc8fbe24b078667e8ef96c4f49dfd3db
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-07-20 16:59:39 +0100

    fix betty style

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -11,10 +13,10 @@
  */
 struct binary_tree_s
 {
-	    int n;
-	        struct binary_tree_s *parent;
-		    struct binary_tree_s *left;
-		        struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -45,4 +47,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
+
 #endif
